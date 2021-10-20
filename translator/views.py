0a036b867672dc5d89e5613fd59409de4d2cb3e0
Evelina Spasova <spasova.evelina@gmail>
2021-10-20 17:40:51 +0300

    connecting with google transaltor

--- translator/views.py
+++ translator/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from . import translator
 
 # Create your views here.
 
 def translator_view(request):
     if request.method == 'POST':
         original_text = request.POST['my_text']
-        output = original_text.upper()
+        output = translator.translate(original_text)
         return render(request, 'translator.html', {'input_text': original_text, 'output_text': output})
     else:
         return render(request, 'translator.html')
