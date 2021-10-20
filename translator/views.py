bb32a3d1838e3c8175abdc2311f1ea48fae2533e
Evelina Spasova <spasova.evelina@gmail>
2021-10-20 17:26:05 +0300

    Getting and Processing User Input Through a Form

--- translator/views.py
+++ translator/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def translator_view(request):
-    return render(request, 'translator.html')
+    if request.method == 'POST':
+        original_text = request.POST['my_text']
+        output = original_text.upper()
+        return render(request, 'translator.html', {'input_text': original_text, 'output_text': output})
+    else:
+        return render(request, 'translator.html')
